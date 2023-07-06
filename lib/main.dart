921ae1ba857910be3ce06992c7a8db7abdb8a709
Eslam Zoghla <95759229+eslamzoghla@users.noreply.github.com>
2023-07-06 19:59:00 +0300

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:revision/XOgames/xogame.dart';
+
+import 'massenger/massenger_screen.dart';
+
+void main() {
+  runApp(const My_app());
+}
+
+class My_app extends StatelessWidget {
+  const My_app({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: XOGame(),
+    );
+  }
+}
